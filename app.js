b6b89b88765b7af4b8926d1963ebfb367be59046
Daniel Allen <danielallen1976@gmail.com>
2021-10-21 05:15:56 -0500

    Thursday Morning

---
+++ app.js
@@ -0,0 +1,23 @@
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
+  
+  promptUser().then(answers => console.log(answers));
+
+/*inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));*/
